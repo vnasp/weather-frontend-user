910d7040cabdccca4b6a394ceb16eeaee003246e
Valentina <valentinamr@gmail.com>
2024-04-20 21:03:59 -0400

    Add search list, delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,78 @@
-import { Box, Container, Typography, TextField } from "@mui/material";
-import { LoadingButton } from "@mui/lab";
-import axios from "axios";
-import { useState } from "react";
-import Weather from "./components/Weather";
+import { useState } from "react"
+import axios from "axios"
+import Weather from "./components/Weather"
+import {
+  Box, Container, Typography, TextField, List, ListItemButton, ListItemText
+} from "@mui/material"
 
 export default function App() {
-  const [weather, setWeather] = useState({});
-  const [city, setCity] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState({
-    error: false,
-    message: "",
-  });
+    const [weather, setWeather] = useState({})
+    const [city, setCity] = useState("")
+    const [suggestions, setSuggestions] = useState([])
+    const [error, setError] = useState({
+        error: false,
+        message: "",
+    })
 
-  const API_WEATHER = "http://api.weatherapi.com/v1/current.json?key=";
-  const TOKEN = VITE_API_KEY
-  const SEARCH = "&lang=es&q=";
+    const API_SEARCH = "https://api.weatherapi.com/v1/search.json?key="
+    const API_WEATHER = "https://api.weatherapi.com/v1/current.json?key="
+    const TOKEN = import.meta.env.VITE_API_KEY
 
-  const SubmitEvent = async (event) => {
-    event.preventDefault();
-    setLoading(true);
-    setError({
-      error: false,
-      message: "",
-    });
-    if (city.trim() !== "" && city.trim().length > 2) {
-      try {
-        const response = await axios.get(API_WEATHER + TOKEN + SEARCH + city);
-        const data = response.data;
-        setWeather({
-          city: data.location.name,
-          country: data.location.country,
-          temp: data.current.temp_c,
-          condition: data.current.condition.code,
-          icon: data.current.condition.icon,
-          conditionText: data.current.condition.text,
-          dia: data.current.is_day,
-        });
-        setCity("");
-      } catch (error) {
-        console.error(error);
-        setError({ error: true, message: "Error al cargar los datos del clima." });
-      } finally {
-        setLoading(false);
-      }
-    } else {
-      setError({ error: true, message: "Ingresa una ciudad válida" });
-      setLoading(false);
+    const handleSearchChange = async (event) => {
+        const value = event.target.value
+        setCity(value)
+        if (value.length > 2) {
+            try {
+                const response = await axios.get(API_SEARCH + TOKEN + "&q=" + value)
+                setSuggestions(response.data)
+                setError({ error: false, message: "" })
+            } catch (error) {
+                console.error('Failed to fetch suggestions:', error)
+                setError({ error: true, message: "Error al cargar las sugerencias." })
+            }
+        } else {
+            setSuggestions([])
+            setError({ error: false, message: "" })
+        }
     }
-  };
-  
+
+    const handleSuggestionClick = async (cityName) => {
+      setCity(cityName)
+      setSuggestions([])
+
+      try {
+          const response = await axios.get(API_WEATHER + TOKEN + "&lang=es&q=" + cityName)
+          setWeather({
+              city: response.data.location.name,
+              country: response.data.location.country,
+              temp: response.data.current.temp_c,
+              condition: response.data.current.condition.code,
+              icon: response.data.current.condition.icon,
+              conditionText: response.data.current.condition.text,
+              dia: response.data.current.is_day,
+          })
+          setCity("")
+      } catch (error) {
+          console.error('Failed to fetch weather data:', error)
+          setError({ error: true, message: "No se pudo cargar la información del clima." })
+      } 
+  }
+
   return (
-    <Container maxWidth="xs" sx={{ mt: "2rem", width:"100vw"}}>
-      <Box component={"div"} display="flex" justifyContent="center">
+    <Container component={"main"}
+    maxWidth={false}
+    sx={{
+      backgroundImage: 'url("./background.webp")',
+      backgroundSize: "cover",
+      backgroundPosition: "center 10%",
+      height: "100vh",
+      width: "100vw",
+      p: 0,
+      m: 0,
+    }}
+    >
+  
+      <Box maxWidth="xs" component={"div"} display="flex" justifyContent="center">
         <img src="./logo.webp" alt="CLIMApp" height={55} />
         <Typography
           variant="h3"
@@ -79,24 +99,24 @@ export default function App() {
           label="Ciudad, País"
           variant="outlined"
           color="success"
-          size="small"
+          size="large"
           value={city}
-          onChange={(event) => setCity(event.target.value)}
+          onChange={handleSearchChange}
           error={error.error}
           helperText={error.message}
+          sx={{ borderRadius: 2, mb:0 }}
         />
-        <LoadingButton
-          type="submit"
-          variant="contained"
-          loading={loading}
-          loadingIndicator="Cargando..."
-          color="success"
-          size="large"
-        >
-          Buscar
-        </LoadingButton>
+        {suggestions.length > 0 && (
+          <List component="nav" sx={{ bgcolor: "background.paper", borderRadius:2 }}>
+            {suggestions.map((item, index) => (
+              <ListItemButton key={index} onClick={() => handleSuggestionClick(item.name + ", " + item.country)}>
+                <ListItemText primary={item.name + ", " + item.country} />
+              </ListItemButton>
+            ))}
+          </List>
+        )}
       </Box>
       <Weather weather={weather} />
     </Container>
-  );
+  )
 }
